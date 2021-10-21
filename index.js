02f0482461b61f44a78b021b688fe920f38325a5
jonathansmurray <jonathansmurray@gmail.com>
2021-10-21 14:52:11 -0700

    commit tree 4014d0f43645765d92f48b6e32c3672f91d3cd9b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+        return string.toLowerCase() === driver.toLowerCase()
+
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    let matchingletters = 
+        drivers.filter(beginningLetters => 
+        beginningLetters.substring(0,2) === string) 
+        return matchingletters 
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name === name
+    })
+}
